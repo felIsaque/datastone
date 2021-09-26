5c69351930623d1c09a95c7f32587f30dd6c051c
Isaque Felizardo <isaque.developer@protonmail.com>
2021-09-26 17:21:54 -0300

    configurando yml de produção

--- up.sh
+++ up.sh
@@ -4,4 +4,9 @@ ENV_APP="$1"
 
 sed -i "s/DEB.*/DEB=${ENV_APP}/" .env
 
-docker-compose up
+if [ $ENV_APP == "FALSE" ]
+then
+    docker-compose up
+else
+    docker-compose -f docker-compose.prod.yml up
+fi
